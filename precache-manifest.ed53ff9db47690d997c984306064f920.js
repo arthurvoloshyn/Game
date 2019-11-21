85bc60d819ec9eb285ef2cf426a9d88435a2a0de
Artur <grandfaraon93@gmail.com>
2019-11-21 16:16:07 +0200

    Updates

---
+++ precache-manifest.ed53ff9db47690d997c984306064f920.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b11a965924be24f0474",
+    "url": "/Game/static/js/runtime~main.4b11a965.js"
+  },
+  {
+    "revision": "5b2849e33634b4bea8a4",
+    "url": "/Game/static/js/main.5b2849e3.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/Game/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "5b2849e33634b4bea8a4",
+    "url": "/Game/static/css/main.c2a6190d.chunk.css"
+  },
+  {
+    "revision": "c5c1c55b048525fc53f02fb8d54ef3dc",
+    "url": "/Game/index.html"
+  }
+];
